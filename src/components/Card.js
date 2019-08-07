46cfa5353b03776c9252002671fe5d20cfc863cc
Stephanie Jones <stephanie.jones@flatiron.com>
2019-08-07 11:41:33 -0600

    Edited Card component for future lessons

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,29 +2,31 @@
 import React from 'react';
 
 class Card extends React.Component {
-    constructor(props) {
-        super(props);
+    //component needs to take in props and hold state
 
-        this.state = {
-            showAnswer: false
-        };
-    }
-
-
+    // function to handle onclick event listener
     clickForAnswer = (event) => {
         event.preventDefault()
-        this.setState({ showAnswer: !this.state.showAnswer })
+        //setstate onclick
     }
     render() {
 
         return (
             <div className="card">
                 <img className="card-img-top" src="http://www.sporcle.com/blog/wp-content/uploads/2018/10/1-48.jpg" alt="Card image cap" />
-                <div className="card-body" key={ this.props.id }>
-                    <h5 className="card-title">Question #{ this.props.id }</h5>
+                {/* use props give div below a key equal to the question id */ }
+                <div className="card-body" >
+                    {/* use props to render question number by rendering question id as question number below */ }
+                    <h5 className="card-title">Question # </h5>
+
+                    {/* use props to render question text in <p>tag</p> */ }
                     <p className="card-text">{ this.props.question }</p>
-                    <a href="#" onClick={ this.clickForAnswer } className="btn btn-primary">Show Answer</a>
-                    { this.state.showAnswer === true ? <h6>{ this.props.answer }</h6> : null }
+
+                    {/* add event listener in <a> tag</a> */ }
+                    <a href="#" className="btn btn-primary">Show Answer</a>
+
+                    {/* ternary to render answer to trivia question in <h6> or <p> tag */ }
+
                 </div>
             </div>
 
