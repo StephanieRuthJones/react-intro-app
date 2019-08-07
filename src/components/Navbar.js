3e520b77f6677b528f0aa4fc38253a4e09b51366
StephanieRuthJones <35548270+StephanieRuthJones@users.noreply.github.com>
2019-08-07 11:32:51 -0600

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,31 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import About from './About'
+import App from '../App'
 
 const NavBar = (props) => {
     return (
+        <Router>
+            <div className="nav-div">
+                <nav className="navbar navbar-expand-lg navbar-light bg-light row-fluid">
+                    <a className="navbar-brand" href="#">Trivia Extravaganza!!!!!</a>
 
-        <nav className="navbar navbar-expand-lg navbar-light bg-light row-fluid">
-            <a className="navbar-brand" href="#">Trivia Extravaganza!!!!!</a>
 
+                    <ul className="navbar-nav">
+                        <li className="nav-item active">
+                            <Link className="nav-link" to="/trivia">Trivia</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/about/">About</Link>
+                        </li>
+                    </ul>
+                </nav>
 
-            <ul className="navbar-nav">
-                <li className="nav-item active">
-                    <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">About</a>
-                </li>
-            </ul>
-        </nav>
+                <Route path="/trivia/" exact component={ App.js } />
+                <Route path="/about/" component={ About } />
+
+            </div>
+        </Router>
     );
 }
 
